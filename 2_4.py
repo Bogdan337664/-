59605efc1cb04f24fc2d4d28a5689917aaf9f6d4
Bogdan337664 <bogdanbelov18@gmail.com>
2024-05-20 21:12:25 +0500

    Update 2_4.py

--- 2_4.py
+++ 2_4.py
@@ -1,39 +1,11 @@
-class Airline:
-    aircrafts = []
-    routes = []
+class X:
+    def __init__(self, a, b):
+        if a != 'Николай':
+            self.a = 'Я не ' + a + ', а Николай'
+        else:
+            self.a = a
+        self.b = b
+        print(self.a)
 
-    def register_aircraft(self, aircraft):
-        self.aircrafts.append(aircraft)
-        print(self.aircrafts)
-        return self.aircrafts
+y = X('МАКСДАУН', -4)
 
-    def deregister_aircraft(self, aircraft):
-        self.aircrafts.remove(aircraft)
-        print(self.aircrafts)
-        return self.aircrafts
-
-    def add_route(self, route):
-        self.routes.append(route)
-        print(self.routes)
-        return self.routes
-
-    def remove_route(self, route):
-        self.routes.remove(route)
-        print(self.routes)
-        return self.routes
-
-    def search_aircraft(self, model):
-        matching_aircrafts = [aircraft for aircraft in self.aircrafts if model == aircraft]
-        print(*matching_aircrafts)
-        return matching_aircrafts
-
-    def search_route(self, city):
-        matching_routes = [route for route in self.routes if city in route]
-        print(*matching_routes)
-        return matching_routes
-
-skyline = Airline()
-skyline.register_aircraft('bz')
-skyline.add_route('a - b')
-skyline.add_route('d - f')
-skyline.search_route('d')

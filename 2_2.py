bc0847216025d5a53c361846d6fd1730f564cb13
Bogdan337664 <bogdanbelov18@gmail.com>
2024-05-20 21:03:01 +0500

    Create 2_2.py

---
+++ 2_2.py
@@ -0,0 +1,10 @@
+class X:
+    def __init__(self, a, b):
+        if a != 'Николай':
+            self.a = 'Я не ' + a + ', а Николай'
+        else:
+            self.a = a
+        self.b = b
+        print(self.a)
+
+y = X('МАКСДАУН', -4)

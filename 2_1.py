b6ea66f27db7984c1c029d3fa85a2683c9df9270
Bogdan337664 <bogdanbelov18@gmail.com>
2024-05-20 21:01:46 +0500

    Create 2_1.py

---
+++ 2_1.py
@@ -0,0 +1,11 @@
+# Привет, Богдан! Ниже приведен переписанный код с измененной структурой, названиями переменных и функций
+
+class LibraryBook:
+    title = None
+    author = None
+    
+    def set_title(self, title):
+        self.title = title
+
+    def set_author(self, author):
+        self.author = author

b96ffa5675f9226be20f92fa5c9b52cd0b34d872
Bogdan337664 <bogdanbelov18@gmail.com>
2024-05-20 20:54:36 +0500

    Update 1_2.py

--- 1_2.py
+++ 1_2.py
@@ -1,1 +1,15 @@
 
+input_a, input_b = int(input()), int(input())
+result_array = list()
+for num in range(input_a, input_b+1):
+    if num > 1:
+        prime_flag = True
+        for divisor in range(2, num):
+            if num % divisor == 0:
+                prime_flag = False
+                break
+        if prime_flag:
+            result_array.append(num)
+    else:
+        continue
+print(sum(result_array))
